f6fae36d1f9a6697c3f439908dbdab84e10d14de
roroastro <roroastro1@gmail.com>
2024-08-29 21:43:02 -0300

    react

---
+++ Aulas/aula3/src/App.jsx
@@ -0,0 +1,18 @@
+function Cabecalho() {
+  return (
+    <header>
+      <h1>Minha Aplicação</h1>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Cabecalho />
+      <p>Bem vindo</p>
+    </>
+  );
+}
+
+export default App;
