2b1ac4b2fc4a3e6164bbf321f53b9a127cbd30a4
roroastro <roroastro1@gmail.com>
2024-09-02 20:09:21 -0300

    Adicionando arquivos da pratica04

---
+++ praticas/pratica04/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+   <div className="card">
+     <Header/>
+     <p>Este é um parágrafo em JSX!</p>
+   </div>
+  );
+}
+
+
+export default App;
+
